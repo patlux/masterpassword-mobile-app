5449520a7b4c4ef26347b743db73319f4f04f3eb
Patrick Wozniak <email@patwoz.de>
2019-09-15 16:18:04 +0200

    feat(import): experimenting with picker and reader

--- App.tsx
+++ App.tsx
@@ -1,10 +1,39 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+import * as DocumentPicker from 'expo-document-picker';
+import * as FileSystem from 'expo-file-system';
 
 export default function App() {
+  const [file, setFile] = React.useState<DocumentPicker.DocumentResult>(null);
+  const [content, setContent] = React.useState(null);
+
+  async function onPress() {
+    const result = await DocumentPicker.getDocumentAsync({
+      type: '*/*',
+      copyToCacheDirectory: true,
+    });
+    setFile(result);
+  }
+
+  React.useEffect(() => {
+    if (!file) {
+      return;
+    }
+    FileSystem.readAsStringAsync(file.uri, { encoding: 'utf8' }).then(
+      result => {
+        setContent(result);
+      },
+    );
+  }, [file]);
+
+  console.log({ file, content });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {file && <Text>Filename: {file.name}</Text>}
+
+      <Button onPress={onPress} title="Select a file"></Button>
     </View>
   );
 }
