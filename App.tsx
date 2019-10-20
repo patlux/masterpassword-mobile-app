87ccc3de2a1228bdc9696d384e18bafe1f153d99
Patrick Wozniak <email@patwoz.de>
2019-10-20 16:33:04 +0200

    feat(statusbar): set dark content for statusbar

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import '@expo/browser-polyfill';
 import React from 'react';
+import { StatusBar } from 'react-native';
 
 import AppProviders from './src/AppProviders';
 import Root from './src';
@@ -8,6 +9,7 @@ export default function App() {
   console.log('App', 'render()');
   return (
     <AppProviders>
+      <StatusBar barStyle="dark-content" />
       <Root />
     </AppProviders>
   );
